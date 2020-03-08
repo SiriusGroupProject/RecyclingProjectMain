30e04066d387b34c9ccf6ffb5faf0d3cb7c9ddd3
eneskolasin <eneskolasin156@gmail.com>
2020-03-08 12:35:23 +0300

    Start logging

--- src/main/java/com/sirius/web/service/UserServiceImpl.java
+++ src/main/java/com/sirius/web/service/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.sirius.web.service;
 import com.sirius.web.model.User;
 import com.sirius.web.repository.UserRepository;
 import com.sirius.web.utils.HashingForPassword;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,7 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     private final UserRepository userRepository;
 
     @Autowired
