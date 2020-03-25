214f4de2c87700334204cdf8f1429f57c08fffc7
gizemelove <gizemelove06@gmail.com>
2020-03-25 22:25:46 +0300

    Notification and location connection bugfixed.

--- src/main/java/com/sirius/web/utils/NotificationClient.java
+++ src/main/java/com/sirius/web/utils/NotificationClient.java
@@ -5,7 +5,7 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class NotificationClient {
-    public static void notificate(String subAdminArea) throws UnirestException {
+    public static void notificate(String subAdminArea,String activateOrAdd) throws UnirestException {
         HttpResponse httpResponse = Unirest.get("http://192.168.2.242:8080/send/{subAdminArea}").asString();
     }
 }
